0ca21dc131f0da2374036b050b5e4cf764e7015d
Kenna <kennaalastair@gmail.com>
2019-10-14 14:36:21 -0700

    tests render all components successfully

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,11 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Display from './Display';
+
+describe('<Display />', () => {
+    it('renders successfully', () => {
+        render(<Display />);
+    })
+})
